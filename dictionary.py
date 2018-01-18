9f68a3d8e1eb88d6998ffd97658a1ebb79da6fb9
Maxwell Krause <maxwell.l.krause@gmail.com>
2018-01-18 04:25:22 -0500

    more functions and pretty printing

--- dictionary.py
+++ dictionary.py
@@ -18,9 +18,11 @@ def input_dialog():
 def get_definition(userinput):
     word = userinput.lower()
     if word in __dictionary__:
-        return __dictionary__[word]
+        pretty_print_defintions(__dictionary__[word])
+        return input_dialog()
     elif len(get_close_matches(word, __dictionary__.keys())) > 0:
-        return handle_matches(word, __dictionary__)
+        handle_matches(word, __dictionary__)
+        return input_dialog()
     else:
         print('Sorry we could not define that word')
         return input_dialog()
@@ -30,13 +32,23 @@ def handle_matches(matchInput, dictionary):
     matchlen = len(matches)
     if matchlen != 0:
         newinput = input('Did you mean %s ? Enter Y or N') % matches[0]
-          return newinput
+        newinput.lower()
         if newinput == 'n':
           matches.pop(0)
           return newinput
+        elif newinput == 'y':
+            defintions = dictionary[matches[0]
+            return pretty_print_defintions(defintions)
         else:
-            return matches[0]
+          print('Please enter Y or N')
+          return handle_matches(matchInput, dictionary)
     else:
-      print('Sorry we did could not find a match')
+      print('Sorry we did not find a match')
       return input_dialog()
 
+def pretty_print_defintions(definition):
+  if isinstance(definition, list):
+      for item in definition:
+          print(item)
+  else:
+      print(definition)
